ff5b4f46ecf16e8e515dd4fcdb593c42af9b9eee
Gilteun Choi <gilteunchoi@gmail.com>
2021-07-25 17:57:40 +0900

    allow all hosts

--- sss/settings.py
+++ sss/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = grab_secrets("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
