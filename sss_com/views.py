a95723b9323223643db94169cc38e475f07503b5
Gilteun Choi <gilteunchoi@gmail.com>
2021-08-18 22:04:08 +0900

    getIP

--- sss_com/views.py
+++ sss_com/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from datetime import datetime
+from ipware import get_client_ip
 
 @api_view(['GET', 'POST'])
 def index(request):
-    print()
-    print("Request:", request)
+    print("IP:", get_client_ip(request))
     query_params = request.query_params.dict()
     body_data = json.loads(request.body)
     print("Params:", query_params)
